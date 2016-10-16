11c087770b673ce8105ac830c280701a406dfae2
Jesse Renee Beach <splendidnoise@gmail.com>
2016-10-16 00:21:16 -0700

    Move the heap console logs into their own functions

--- heap.js
+++ heap.js
@@ -15,7 +15,7 @@ class BinaryHeap {
     let end = this.heap.pop();
     if (this.heap.length > 0) {
       this.heap[0] = end;
-      this._sink(0, result);
+      this._sink(0);
     }
     return result;
   }
@@ -28,10 +28,10 @@ class BinaryHeap {
           break;
         }
         this.heap[i] = end;
-        console.log('Bubble: ');
+        __log('Remove bubble: ');
         this._bubble(i);
-        console.log('Sink: ');
-        this._sink(i, end);
+        __log('Remove Sink: ');
+        this._sink(i);
         break;
       }
     }
@@ -41,15 +41,7 @@ class BinaryHeap {
   }
   _bubble(index) {
     let el = this.heap[index];
-    console.log(
-      [
-        '[',
-        this.heap.map(
-          (el, i) => (i === index) ? colors.green(el) : el
-        ).join(', '),
-        ']'
-      ].join('')
-    );
+    __logBubbleStart(this.heap, index);
     while (index > 0) {
       let parentIndex = Math.floor((index + 1) / 2) - 1;
       let parentEl = this.heap[parentIndex];
@@ -58,21 +50,11 @@ class BinaryHeap {
       }
       this.heap[parentIndex] = el;
       this.heap[index] = parentEl;
-      console.log(
-        [
-          '[',
-          this.heap.map(
-            (el, i) =>
-              (i === parentIndex) ? colors.green(el) :
-                (i === index) ? colors.yellow.underline(parentEl) : el
-          ).join(', '),
-          ']'
-        ].join('')
-      );
+      __logBubbleWhile(this.heap, index, parentIndex, parentEl);
       index = parentIndex;
     }
   }
-  _sink(index, result) {
+  _sink(index) {
     let len = this.heap.length;
     let el = this.heap[index];
 
@@ -97,35 +79,11 @@ class BinaryHeap {
       }
       // No swap to make.
       if (swapIndex == null) {
-        console.log(
-          [
-            '[',
-            this.heap.map(
-              (el, i) => (i === index) ? colors.green(el) :
-               (i === leftChildIndex) ? colors.yellow(el) :
-                 (i === rightChildIndex) ? colors.red(el) : el
-            ).join(', '),
-            ']'
-          ].join('')
-        );
+        __logSinkNoSwap(this.heap, index, leftChildIndex, rightChildIndex);
         break;
       }
       // Make a swap.
-      console.log(
-        [
-          '[',
-          this.heap.map(
-            (el, i) => (i === index) ? colors.green(el) :
-             (i === leftChildIndex) ? (
-               (i === swapIndex) ? colors.yellow.underline(el) : colors.yellow(el)
-             ) :
-               (i === rightChildIndex) ? (
-                 (i === swapIndex) ? colors.red.underline(el) : colors.red(el)
-               ) : el
-          ).join(', '),
-          ']'
-        ].join('')
-      );
+      __logSinkSwap(this.heap, index, leftChildIndex, rightChildIndex, swapIndex);
       this.heap[index] = this.heap[swapIndex];
       this.heap[swapIndex] = el;
       index = swapIndex;
@@ -146,3 +104,65 @@ console.log('**** Remove 2 ****');
 minHeap.remove(15);
 console.log('**** Pop all ****');
 input.forEach(x => minHeap.pop());
+
+function __log(str) {
+  console.log(str);
+}
+
+function __logBubbleStart(heap, index) {
+  console.log(
+    [
+      '[',
+      heap.map(
+        (el, i) => (i === index) ? colors.green(el) : el
+      ).join(', '),
+      ']'
+    ].join('')
+  );
+}
+
+function __logBubbleWhile(heap, index, parentIndex, parentEl) {
+  console.log(
+    [
+      '[',
+      heap.map(
+        (el, i) =>
+          (i === parentIndex) ? colors.green(el) :
+            (i === index) ? colors.yellow.underline(parentEl) : el
+      ).join(', '),
+      ']'
+    ].join('')
+  );
+}
+
+function __logSinkNoSwap(heap, index, leftChildIndex, rightChildIndex) {
+  console.log(
+    [
+      '[',
+      heap.map(
+        (el, i) => (i === index) ? colors.green(el) :
+         (i === leftChildIndex) ? colors.yellow(el) :
+           (i === rightChildIndex) ? colors.red(el) : el
+      ).join(', '),
+      ']'
+    ].join('')
+  );
+}
+
+function __logSinkSwap(heap, index, leftChildIndex, rightChildIndex, swapIndex) {
+  console.log(
+    [
+      '[',
+      heap.map(
+        (el, i) => (i === index) ? colors.green(el) :
+         (i === leftChildIndex) ? (
+           (i === swapIndex) ? colors.yellow.underline(el) : colors.yellow(el)
+         ) :
+           (i === rightChildIndex) ? (
+             (i === swapIndex) ? colors.red.underline(el) : colors.red(el)
+           ) : el
+      ).join(', '),
+      ']'
+    ].join('')
+  );
+}
